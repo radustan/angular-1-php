da05aac5dcb23e86ec7c905dfc69e9419d12d91d
radustan <raduadrian.stan@gmail.com>
2016-03-08 12:56:03 +0200

    Config

--- application/controllers/Angular.php
+++ application/controllers/Angular.php
@@ -30,6 +30,7 @@ class Angular extends MY_Controller {
         $this->load->model('Contacts','contacts');
         $data = $this->contacts->getAllArray();
         header('Content-Type: application/json');
+        header("Access-Control-Allow-Origin: *");
         echo json_encode($data);
     }
 
