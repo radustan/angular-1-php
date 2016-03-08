260d48c38609cce5133c27b95ca95ce481282eec
radustan <raduadrian.stan@gmail.com>
2016-03-08 12:38:42 +0200

    update

--- application/models/Contacts.php
+++ application/models/Contacts.php
@@ -3,5 +3,16 @@
 class Contacts extends MY_Model
 {
     protected $tableName = 'messages';
+
+    public function getAllArray()
+    {
+        return array(
+            array(
+                'name' => 'radu',
+                'email' => 'radu.stan@ggg.com',
+                'message' => 'hello'
+            )
+        );
+    }
 }
 
